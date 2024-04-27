b1bbd1cd5aa1279cdfdb8016d2f5de58b3f3bd8a
rushilpatra <70966802+rushilpatra@users.noreply.github.com>
2024-04-27 13:32:18 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from langchain_community.document_loaders import HuggingFaceDatasetLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+
+model_name = "all-MiniLM-L6-v2"
+
+
+embeddings = HuggingFaceEmbeddings(model_name=model_name)
+new_db = FAISS.load_local("medical index", embeddings, allow_dangerous_deserialization=True)
+st.title('Medical Question Answering')
+question = st.text_input("Enter your medical question:")
+if question:
+    retriever = new_db.as_retriever(search_kwargs={"k": 3})
+    context = retriever.get_relevant_documents(question)
+    st.write("Relevant documents:")
+    st.write(context)
