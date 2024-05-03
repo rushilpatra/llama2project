3f2b2e637b93f61cea8e28b29f7ee2db65790af3
rushilpatra <70966802+rushilpatra@users.noreply.github.com>
2024-05-03 16:17:09 +0530

    Add files via upload

--- project.py
+++ project.py
@@ -3,14 +3,47 @@ from langchain_community.document_loaders import HuggingFaceDatasetLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
+from unsloth import FastLanguageModel
+import torch
+max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "rushilpatra/medical_lora_model",
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+)
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
 model_name = "all-MiniLM-L6-v2"
 embeddings = HuggingFaceEmbeddings(model_name=model_name)
 new_db = FAISS.load_local("medical index", embeddings, allow_dangerous_deserialization=True)
-st.title('Medical Chat Bot')
-question = st.text_input("Ask me a question")
+st.title('Medical Question Answering')
+question = st.text_input("Enter your medical question:")
 if question:
-    retriever = new_db.as_retriever(search_kwargs={"k": 5})
+    retriever = new_db.as_retriever(search_kwargs={"k": 1})
     context = retriever.get_relevant_documents(question)
+    FastLanguageModel.for_inference(model) # Enable native 2x faster inference
+    inputs = tokenizer(
+    [
+        alpaca_prompt.format(
+            f"{question} and context:{context}", # instruction
+            "", # input
+            "", # output - leave this blank for generation!
+        )
+    ], return_tensors = "pt").to("cuda")
+
+outputs = model.generate(**inputs, max_new_tokens = 64, use_cache = True)
+tokenizer.batch_decode(outputs)
     st.write("Relevant documents:")
     st.write(context)
